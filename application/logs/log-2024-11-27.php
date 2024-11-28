329a484b6e6753342c5064e9dc92333184308f1d
kxsong8745 <lablol1997@gmail.com>
2024-11-28 09:38:52 +0800

    First commit

---
+++ application/logs/log-2024-11-27.php
@@ -0,0 +1,76 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-11-27 04:29:53 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 04:33:43 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 04:33:43 --> Severity: error --> Exception: syntax error, unexpected identifier "publick", expecting "function" or "const" /var/www/html/public/application/modules/manage/controllers/Booking.php 12
+ERROR - 2024-11-27 04:33:54 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 04:39:50 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 04:39:51 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 04:40:48 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 04:49:24 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 04:50:15 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 04:50:15 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 04:50:15 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 04:50:15 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 04:50:15 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 04:50:15 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 04:50:15 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 04:51:26 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 04:51:27 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 04:51:27 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 04:51:27 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 04:51:27 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 04:51:27 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 04:51:27 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 04:51:27 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 04:51:27 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 04:51:54 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 04:51:54 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 04:51:54 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 04:51:54 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 04:51:54 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 04:51:54 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 04:51:54 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 04:53:37 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 04:53:37 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 04:53:37 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 04:53:37 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 04:53:37 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 04:53:37 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 04:53:37 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 04:53:37 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 04:53:37 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 05:01:31 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 05:01:32 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 05:01:32 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 05:01:32 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 05:01:32 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 05:01:32 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 05:01:32 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 05:01:32 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 05:01:32 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 05:05:47 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 05:05:47 --> Could not find the language line "cov_bank_kod"
+ERROR - 2024-11-27 05:05:47 --> Could not find the language line "ccc_delete_confirm"
+ERROR - 2024-11-27 05:05:47 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 05:05:47 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 05:05:47 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 05:05:47 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 05:05:47 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 05:05:47 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 05:13:41 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 05:13:41 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 05:13:41 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 05:13:41 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 05:13:41 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 05:13:41 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 05:13:41 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 05:17:48 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 05:17:48 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 05:17:48 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 05:17:48 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 05:17:48 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 05:17:48 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 05:17:48 --> 404 Page Not Found: /index
+ERROR - 2024-11-27 05:17:48 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2024-11-27 05:17:48 --> 404 Page Not Found: /index
