2b2cf9d2655d7f0b548d334722a8e4e2d1978082
kxsong8745 <lablol1997@gmail.com>
2025-05-23 10:06:42 +0800

    edit sidebar items

---
+++ application/logs/log-2025-05-22.php
@@ -0,0 +1,204 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2025-05-22 03:27:15 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:27:17 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:27:17 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:27:18 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:27:18 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:27:18 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:27:18 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:28:37 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:28:39 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:28:39 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:28:39 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:28:39 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:28:39 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:28:39 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:28:44 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:28:45 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:28:45 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:28:45 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:28:45 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:28:45 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:28:45 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:29:10 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:29:11 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:29:11 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:29:11 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:29:11 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:29:12 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:29:12 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:29:14 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:29:16 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:29:16 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:29:16 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:29:16 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:29:16 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:29:16 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:29:21 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:29:22 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:29:22 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:29:22 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:29:22 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:29:22 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:29:23 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:29:30 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:30:15 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:30:18 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:30:19 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:30:19 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:30:19 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:30:19 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:30:19 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:30:19 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:32:11 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:32:12 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:32:12 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:32:12 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:32:12 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:32:12 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:32:12 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:32:15 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:32:22 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:32:23 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:32:23 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:32:23 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:32:23 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:32:23 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:32:24 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:32:24 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:32:32 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:32:33 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:32:33 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:32:33 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:32:33 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:32:33 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:32:33 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:32:42 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:32:43 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:32:43 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:32:43 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:32:43 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:32:43 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:32:43 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:32:54 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:32:55 --> Severity: Warning --> Undefined property: stdClass::$next_id /var/www/html/public/application/modules/ipss/models/Prepdisp_model.php 103
+ERROR - 2025-05-22 03:32:55 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:32:56 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:32:56 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:32:56 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:32:56 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:32:56 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:32:56 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:33:09 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:33:10 --> Severity: Warning --> Undefined property: stdClass::$next_id /var/www/html/public/application/modules/ipss/models/Prepdisp_model.php 172
+ERROR - 2025-05-22 03:33:10 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:33:10 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:33:10 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:33:10 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:33:10 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:33:10 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:33:11 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:34:38 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:34:40 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:34:40 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:34:40 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:34:40 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:34:40 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:34:40 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:39:03 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:39:05 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:39:05 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:39:05 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:39:05 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:39:05 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:39:05 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:51:28 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:51:29 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:51:29 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:51:30 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:51:30 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:51:30 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:51:30 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:51:32 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:52:49 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:52:50 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:52:50 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:52:50 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:52:50 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:52:50 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:52:50 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:53:08 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:53:09 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:53:09 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:53:10 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:53:10 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:53:10 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:53:10 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:53:17 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:53:18 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:53:18 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:53:18 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:53:18 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:53:18 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:53:18 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:54:50 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:54:54 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:54:55 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:54:56 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:54:56 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:54:56 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:54:56 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:54:56 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:55:48 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:55:49 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:57:44 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:57:46 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:57:46 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:57:46 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:57:46 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:57:46 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:57:46 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:58:12 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:58:13 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:58:13 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:58:13 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:58:13 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:58:13 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:58:13 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:58:18 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:58:19 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:58:19 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:58:19 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:58:19 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:58:19 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:58:19 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:59:02 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:59:03 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:59:03 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:59:03 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:59:03 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:59:03 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:59:03 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:59:21 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:59:22 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:59:22 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:59:22 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:59:22 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:59:22 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:59:22 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:59:44 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:59:46 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:59:46 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:59:46 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:59:46 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 03:59:46 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 03:59:46 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 04:00:38 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 04:00:41 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 04:00:42 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 04:00:42 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 04:00:42 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 04:00:42 --> 404 Page Not Found: /index
+ERROR - 2025-05-22 04:00:42 --> Could not find the specified $config['composer_autoload'] path: vendor/autoload.php
+ERROR - 2025-05-22 04:00:42 --> 404 Page Not Found: /index
